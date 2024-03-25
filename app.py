966f80d70366efd3b45fec60e19bfa2f5bcdcb58
Serra <136988908+Serurays@users.noreply.github.com>
2024-03-25 07:56:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,47 +3,7 @@ import torch
 from torchvision import transforms
 from PIL import Image
 from torch import nn
-
-class StrokeClassifierCNN(nn.Module):
-   def __init__(self):
-       super(StrokeClassifierCNN, self).__init__()
-       self.conv_block_1 = nn.Sequential(
-           nn.Conv2d(1, 32, kernel_size=3, padding=1),
-           nn.BatchNorm2d(32),
-           nn.ReLU(inplace=True),
-           nn.Conv2d(32, 64, kernel_size=3, padding=1),
-           nn.BatchNorm2d(64),
-           nn.ReLU(inplace=True),
-           nn.MaxPool2d(kernel_size=2, stride=2),
-       )
-
-       self.conv_block_2 = nn.Sequential(
-           nn.Conv2d(64, 64, kernel_size=3, padding=1),
-           nn.BatchNorm2d(64),
-           nn.ReLU(inplace=True),
-           nn.Conv2d(64, 128, kernel_size=3, padding=1),
-           nn.BatchNorm2d(128),
-           nn.ReLU(inplace=True),
-           nn.MaxPool2d(kernel_size=2, stride=2),
-       )
-
-       self.classifier = nn.Sequential(
-           nn.Flatten(),
-           nn.Linear(128*56*56, 128),
-           nn.ReLU(inplace=True),
-           nn.Dropout(p=0.2),
-           nn.Linear(128, 1)
-       )
-
-   def forward(self, x):
-       # x = self.conv_block_1(x)
-       # print(x.shape)
-       # x = self.conv_block_2(x)
-       # print(x.shape)
-       # x = self.classifier(x)
-       # print(x.shape)
-       # return x
-       return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+from stroke_detection_model.py import StrokeClassifierCNN
 
 model = StrokeClassifierCNN()
 model.load_state_dict(torch.load("best_model.pt"))
